9b5eea8680a5cb87545df4093141ab2fe80f5a40
andreResende <andre@300000kms.net>
2020-10-30 15:17:32 +0100

    Requirements

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ setup(
     name='wordPredictor',
     version='0.0.1',
     description='Rank words based on their similarity',
-    url='git@gitlab.com:dedsresende/tm_dash.git',
+    url='git@github.com:dedsresende/wordPredictor.git',
     author='Andre Resende',
     author_email='dedsresende@gmail.com',
     license='unlicense',
     packages=['tm_dash'],
     zip_safe=False,
     install_requires=[
-       'pandas >= 0.24.2',
-       'Shapely >= 1.6.4.post2',
-       'numpy >= 1.13.3',
-       'colormap >= 1.0.3',
-       'matplotlib >= 3.2.2',
-       'plotly >= 4.8.2',
-       'scipy >= 1.5.1'
+        'Unidecode >= 1.1.1',
+        'scikit_learn >= 0.23.2'
     ]
 )
